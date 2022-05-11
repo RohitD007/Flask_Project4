e5e2e6c631d8fd10c094239a6854716783446e45
RohitD007 <dsouza.rohit.94@gmail.com>
2022-05-10 20:13:37 -0400

    Added simple login  application with configuration test

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,16 +1,18 @@
 from datetime import datetime
 
+from sqlalchemy import Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import db
 from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
 
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(300), nullable=False, unique=True)
+    password = db.Column(db.String(300), nullable=False)
     about = db.Column(db.String(300), nullable=True, unique=False)
     authenticated = db.Column(db.Boolean, default=False)
     registered_on = db.Column('registered_on', db.DateTime)
